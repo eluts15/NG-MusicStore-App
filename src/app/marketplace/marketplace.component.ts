07a2f591f8b2976f7ecbf15d3f45cc14f2cfea4a
eluts <eluts15@gmail.com>
2017-07-11 06:33:14 -0700

    begin refactoring marketplace componnent and album components for firebase.

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,7 @@ import { Component , OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseListOvservable } from  'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
@@ -13,7 +14,7 @@ import { AlbumService } from '../album.service';
 
 export class MarketplaceComponent implements OnInit {
 
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private albumService: AlbumService) { }
 
